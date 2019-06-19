ac1ea4ccc9b13f10a8bfdfd22923b446ef77f0c0
YakambramSai <yk07021988@gmail.com>
2019-06-19 21:33:56 +0530

    tested spring boot annotations

--- microservice/src/main/java/com/yk/microservice/MicroserviceApplication.java
+++ microservice/src/main/java/com/yk/microservice/MicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.yk.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = {"com.yk.microservice", "com.yk.microservice1"})
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,15 @@ public class MicroserviceApplication {
 	}
 
 }
+
+
+/*@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.yk.microservice1"})
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+
+} */
--- microservice/src/test/java/com/yk/microservice/MicroserviceApplicationTests.java
+++ microservice/src/test/java/com/yk/microservice/MicroserviceApplicationTests.java
@@ -2,15 +2,33 @@ package com.yk.microservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class MicroserviceApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloGradle() throws Exception {
+        mvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Hello Gradle!"));
+    }
 }
